a6c208fe0234d1d6c486febd508e0aab2231c00a
Patrickyyh <yeyuhao1234@tamu.edu>
2022-10-05 18:47:34 -0500

    data bas

--- routes/signin.js
+++ routes/signin.js
@@ -5,6 +5,7 @@ import User from '../models/User.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+// controller for sign in 
 const router = express.Router();
 router.post('/api/v1/auth/login', async (req,res)=>{
     const {email, password, lastName, firstName} = req.body
