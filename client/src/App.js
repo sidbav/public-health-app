b7781ddd5ed387903856512aebe22e2826ddbf30
watermelonwhat <78194255+JiachengZhao98@users.noreply.github.com>
2022-12-01 13:47:23 -0600

    Google Maps update

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Profile from './components/Profile';
 // import Components
 import Dashboard from './components/Dashboard';
 import AdminNavbar from './components/AdminNavbar.jsx';
-import Maps from './components/Maps';
+import MapSection from './components/GoogleMaps/Map.jsx' // this is for MapSection
 import Surveyone from './components/Surveyone';
 import TableList from './components/TableList';
 
@@ -21,6 +21,19 @@ import "./assets/css/animate.min.css";
 import "./assets/css/demo.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss"
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+
+// do not delete this variable
+// const location = {
+//   address: 'Baylor Scott & White Medical Center – College Station',
+//   lat:30.58254420899421,
+//   lng:-96.27917886514233
+// };
+
+
+// include this to the App function
+//{ <MapSection location={location} zoomLevel={15} />  }
 
 function App() {
   return (
@@ -33,7 +46,6 @@ function App() {
           }>
               <Route path = "/dashboard" element  = {<Dashboard />}/>
               <Route path = "/profile" element  = {<Profile />}/>
-              <Route path = "/maps" element  = {<Maps /> }/>
               <Route path = "/surveys" element  = {<TableList />} />
           </Route>
 
