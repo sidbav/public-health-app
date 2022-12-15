e25b5711078c74509e332d93b18729cc7b74d801
Patrickyyh <yeyuhao1234@tamu.edu>
2022-12-15 08:47:08 -0600

    add some protection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 
 import { errorHandlerMiddle } from './middlewares/error-handler.js'
 // import the router from the routes folder
@@ -12,8 +14,24 @@ import { updateUserRouter } from './routes/Auth/updateUser.js'
 
 // run the router
 const app = express()
+
+/**
+ * asic rate-limiting middleware for Express.
+ * Use to limit repeated requests to public APIs and/or endpoints such as password reset
+ */
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max:100,
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
+
 app.use(cors())
 app.use(express.json());
+app.use(limiter);
+app.use(helmet());
+
 app.use(LoginRouter);
 app.use(SignupRouter);
 app.use(SurveryRouter);
